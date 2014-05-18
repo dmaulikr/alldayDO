3dca83f4d488ebe99deb4768229abca2c0a8cec0
Fábio Nogueira <fabio.nogueira@me.com>
2014-05-18 17:29:13 -0300

    Initial Commit

---
+++ alldayDO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  alldayDO
+//
+//  Created by Fábio Nogueira  on 18/05/14.
+//  Copyright (c) 2014 Fábio Nogueira . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADAppDelegate class]));
+    }
+}
