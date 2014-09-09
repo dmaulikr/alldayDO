4b7bc6463d7f3e4dcc0de82ce32d2db5e5bfbfc5
Fábio Nogueira <fna.contact@gmail.com>
2014-09-09 13:19:08 -0300

    Adicionando elementos na tela de about

---
+++ alldayDO/code/controller/ADAboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  ADAboutViewController.h
+//  alldayDO
+//
+//  Created by Fábio Almeida on 9/9/14.
+//  Copyright (c) 2014 F√°bio Nogueira . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAboutViewController : UIViewController
+
+- (IBAction)fabioTwitterTouched:(id)sender;
+- (IBAction)arthurTwitterTouched:(id)sender;
+
+@end
---
+++ alldayDO/code/controller/ADAboutViewController.m
@@ -0,0 +1,44 @@
+//
+//  ADAboutViewController.m
+//  alldayDO
+//
+//  Created by Fábio Almeida on 9/9/14.
+//  Copyright (c) 2014 F√°bio Nogueira . All rights reserved.
+//
+
+#import "ADAboutViewController.h"
+
+@interface ADAboutViewController ()
+
+@end
+
+@implementation ADAboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+    [tracker set:kGAIScreenName value:@"aboutScreen"];
+    [tracker set:kGAIEventCategory value:@"Screen"];
+    [tracker send:[[GAIDictionaryBuilder createAppView] build]];
+    
+    self.title = NSLocalizedString(@"about", nil);
+}
+
+- (IBAction)fabioTwitterTouched:(id)sender {
+    id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+    [tracker set:kGAIEventAction value:@"fabioTwitterActivity"];
+    [tracker set:kGAIEventCategory value:@"Action"];
+    [tracker send:[[GAIDictionaryBuilder createAppView] build]];
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.twitter.com/fabintk"]];
+}
+
+- (IBAction)arthurTwitterTouched:(id)sender {
+    id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+    [tracker set:kGAIEventAction value:@"arthurTwitterActivity"];
+    [tracker set:kGAIEventCategory value:@"Action"];
+    [tracker send:[[GAIDictionaryBuilder createAppView] build]];
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.twitter.com/arthurklose"]];
+}
+@end
