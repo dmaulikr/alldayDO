b5388e71ad307bfd6555189f169684d0446d0aa4
Fábio Nogueira <fabio.nogueira@me.com>
2014-09-26 01:07:52 -0300

    Novas Imagens para o passo a passo Inserindo tradução

--- alldayDO/code/view/ADWalkthrough.m
+++ alldayDO/code/view/ADWalkthrough.m
@@ -32,42 +32,60 @@
     UIImage *bgImage = [UIImage imageNamed:@"bgWalkthrough"];
     
     EAIntroPage *page1 = [EAIntroPage page];
-    page1.title = @"Rápido e prático";
-    page1.desc = @"Poucos cliques e atividades salvas. \n AH! E você também pode preparar uma nova atividade dando um Shake!";
+    page1.title = NSLocalizedString(@"page1Title", nil);
+    page1.desc = NSLocalizedString(@"page1Desc", nil);
     page1.descFont = descFont;
     page1.bgImage = bgImage;
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"shake-pt"]];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:NSLocalizedString(@"shakeImage", nil)]];
     [imageView setW:288.f andH:311.f];
     page1.titleIconView = imageView;
     
     EAIntroPage *page2 = [EAIntroPage page];
-    page2.title = @"Acompanhe sua produtividade";
-    page2.desc = @"Nossos gráficos te ajudam a acompanhar seu desempenho";
+    page2.title = NSLocalizedString(@"page2Title", nil);
+    page2.desc = NSLocalizedString(@"page2Desc", nil);
     page2.descFont = descFont;
     page2.bgImage = bgImage;
-    page2.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title2"]];
+    page2.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:NSLocalizedString(@"detailImage", nil)]];
     
     EAIntroPage *page3 = [EAIntroPage page];
-    page3.title = @"Não perca nenhuma informação";
-    page3.desc = @"Confira todas suas atividades, as que você não fez e as já completadas";
+    page3.title = NSLocalizedString(@"page3Title", nil);
+    page3.desc = NSLocalizedString(@"page3Desc", nil);
     page3.descFont = descFont;
     page3.bgImage = bgImage;
-    page3.titleIconPositionY = 0.f;
-    page3.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"walkthrough"]];
+    page3.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:NSLocalizedString(@"badgesImage", nil)]];
     
     EAIntroPage *page4 = [EAIntroPage page];
-    page4.title = @"E claro, divertido";
-    page4.desc = @"Chega de notificação simples. Queremos que você lembre de forma divertida";
+    page4.title = NSLocalizedString(@"page4Title", nil);
+    page4.desc = NSLocalizedString(@"page4Desc", nil);
     page4.descFont = descFont;
     page4.bgImage = bgImage;
-    page4.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title4"]];
+    page4.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:NSLocalizedString(@"notificationImage", nil)]];
     
     EAIntroPage *page5 = [EAIntroPage page];
-    page5.title = @"Agora é com você!";
-    page5.desc = @"Adicione uma atividade e veja a mágica acontecer!";
+    page5.title = NSLocalizedString(@"page5Title", nil);
+    page5.desc = NSLocalizedString(@"page5Desc", nil);
     page5.descFont = descFont;
     page5.bgImage = bgImage;
-    page5.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title5"]];
+    page5.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:NSLocalizedString(@"mainImage", nil)]];
+    
+//    CGFloat titleIconPositionY = 10.f;
+//    CGFloat titleIconViewHeight = 20.f;
+//    page1.titleIconPositionY = titleIconPositionY;
+//    page2.titleIconPositionY = titleIconPositionY;
+//    page3.titleIconPositionY = titleIconPositionY;
+//    page4.titleIconPositionY = titleIconPositionY;
+//    page5.titleIconPositionY = titleIconPositionY;
+//    
+//    [page1.titleIconView setW:(page1.titleIconView.width - titleIconViewHeight)
+//                         andH:(page1.titleIconView.height - titleIconViewHeight)];
+//    [page2.titleIconView setW:(page1.titleIconView.width - titleIconViewHeight)
+//                         andH:(page1.titleIconView.height - titleIconViewHeight)];
+//    [page3.titleIconView setW:(page1.titleIconView.width - titleIconViewHeight)
+//                         andH:(page1.titleIconView.height - titleIconViewHeight)];
+//    [page4.titleIconView setW:(page1.titleIconView.width - titleIconViewHeight)
+//                         andH:(page1.titleIconView.height - titleIconViewHeight)];
+//    [page5.titleIconView setW:(page1.titleIconView.width - titleIconViewHeight)
+//                         andH:(page1.titleIconView.height - titleIconViewHeight)];
 
     self.pages = @[page1, page2, page3, page4, page5];
 }
