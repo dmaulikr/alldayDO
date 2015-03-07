c32a99719ef11e3d1873e91594d1b93c12547db4
Fábio Nogueira de Almeida <fna.contact@gmail.com>
2015-03-07 19:00:27 -0300

    Finalizando implementação de categoria
    Former-commit-id: 729326a760dbd58cb83e87facfd94831e0ba90bc

--- alldayDO/code/controller/ADSettingsTableViewController.m
+++ alldayDO/code/controller/ADSettingsTableViewController.m
@@ -10,9 +10,64 @@
 
 @interface ADSettingsTableViewController ()
 
+@property (nonatomic, strong) ADCategoria *categoriaOne;
+@property (nonatomic, strong) ADCategoria *categoriaTwo;
+@property (nonatomic, strong) ADCategoria *categoriaThree;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsControllerCategoria;
+
+- (void)_addTextToCategoriaLabels;
+- (void)_saveCategorias;
+
 @end
 
 @implementation ADSettingsTableViewController
+
+
+#pragma mark - Getter Methods
+
+- (NSFetchedResultsController *)fetchedResultsControllerCategoria {
+    if (!_fetchedResultsControllerCategoria) {
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"ADCategoria"];
+        request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"descricao" ascending:YES]];
+        
+        _fetchedResultsControllerCategoria = [[NSFetchedResultsController alloc] initWithFetchRequest:request
+                                                                                 managedObjectContext:[ADModel sharedInstance].managedObjectContext
+                                                                                   sectionNameKeyPath:nil
+                                                                                            cacheName:@"descricao_reminders_cache"];
+    }
+    return _fetchedResultsControllerCategoria;
+}
+
+#pragma mark - Private Methods
+
+- (void)_addTextToCategoriaLabels {
+    for (int i = 1; i <= 3; i++) {
+        if (i == 1) {
+            self.categoriaOne = [[self.fetchedResultsControllerCategoria fetchedObjects] objectAtIndex:0];
+            self.categoriaOneText.text = self.categoriaOne.descricao;
+        }
+        if (i == 2) {
+            self.categoriaTwo = [[self.fetchedResultsControllerCategoria fetchedObjects] objectAtIndex:1];
+            self.categoriaTwoText.text = self.categoriaTwo.descricao;
+        }
+        if (i == 3) {
+            self.categoriaThree = [[self.fetchedResultsControllerCategoria fetchedObjects] objectAtIndex:2];
+            self.categoriaThreeText.text = self.categoriaThree.descricao;
+        }
+    }
+}
+
+- (void)_saveCategorias {
+    self.categoriaOne.descricao = self.categoriaOneText.text;
+    self.categoriaTwo.descricao = self.categoriaTwoText.text;
+    self.categoriaThree.descricao = self.categoriaThreeText.text;
+    
+    [[ADModel sharedInstance] saveChanges];
+}
+
+
+#pragma mark - ViewController Methods
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -20,8 +75,18 @@
     [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"navigation_second_color_bg"]
                                                   forBarMetrics:UIBarMetricsDefault];
 
-    #warning TODO - colocar tradução
-    self.title = @"Settings";
+    self.title = NSLocalizedString(@"settings", nil);
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.fetchedResultsControllerCategoria performFetch:nil];
+    [self _addTextToCategoriaLabels];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self _saveCategorias];
 }
 
 @end
